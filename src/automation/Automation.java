e2e9f99ff44d0d4331875849385bc2dca421cf78
adian.juan <>
2017-01-20 17:46:04 +0700

    adding string sql

--- src/automation/Automation.java
+++ src/automation/Automation.java
@@ -1,12 +1,14 @@
 package automation;
 
-public class Automation {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Automation {
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        Date dt = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        String strDate = sdf.format(dt);
+        System.out.println(strDate);
+    }   
 }
